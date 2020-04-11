ae90ad1afbceab4236985de087266f30b21354f8
nikiforoffx <63499133+nikiforoffx@users.noreply.github.com>
2020-04-11 21:23:00 +0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,13 @@
+function init() {
+
+Tabletop.init( 
+{ key: ‘https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+  
+  callback: function(data, tabletop) {
+  console.log(data) },
+    simpleSheet: true 
+    
+    } )
+}
+
+window.addEventListener(‘DOMContentLoaded’, init)
