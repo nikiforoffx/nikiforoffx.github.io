ed983e64853d761366c448bce56dd9e4c5aed9c8
nikiforoffx <63499133+nikiforoffx@users.noreply.github.com>
2020-04-11 23:11:21 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,9 @@
        var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1itDmeN0nb5fdg4-k4HG5zE1hegS8mvwYcXnT0dQOfxM/pubhtml';
-       
-            function init() {
+       var data = [ { name: "Carrot", category: "Vegetable", healthiness: "Adequate" }, 
+                    { name: "Pork Shoulder", category: "Meat", healthiness: "Questionable" }, 
+                    { name: "Bubblegum", category: "Candy", healthiness: "Super High"} ]
+            
+       function init() {
                 Tabletop.init( { key: publicSpreadsheetUrl, callback: showInfo, simpleSheet: true } )
                             }
 
