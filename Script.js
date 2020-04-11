857fb3def33d66bab3d1225604f26bd72025e379
nikiforoffx <63499133+nikiforoffx@users.noreply.github.com>
2020-04-11 22:15:14 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,3 @@
-      <script type='text/javascript'>    
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
-
        var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1itDmeN0nb5fdg4-k4HG5zE1hegS8mvwYcXnT0dQOfxM/pubhtml';
 
             function init() {
@@ -15,4 +12,3 @@
                                               }
             window.addEventListener('DOMContentLoaded', init);
 
-      </script>
