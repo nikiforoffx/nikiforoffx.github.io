299bda7fef64db1b8125acc2381cf2cdb403937b
nikiforoffx <63499133+nikiforoffx@users.noreply.github.com>
2020-04-11 23:38:23 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,18 +1,21 @@
-       var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1itDmeN0nb5fdg4-k4HG5zE1hegS8mvwYcXnT0dQOfxM/pubhtml';
+const Tabletop = require('tabletop'); 
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1itDmeN0nb5fdg4-k4HG5zE1hegS8mvwYcXnT0dQOfxM/pubhtml';
+
+function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: false } )
+  }
+
+ function showInfo(data, tabletop) {
+  // do something with the data
+  console.log(JSON.stringify(data, null, 2));
+}
+
+//initialise and kickstart the whole thing.
+init()
+       
 
             
-       function init() {
-                Tabletop.init( { key: publicSpreadsheetUrl, callback: showInfo, simpleSheet: true } )
-                            }
-
-            function showInfo(data, tabletop) {
-                     alert("Successfully processed!"), console.log(data)
-                                              }
-
-                 
-
-
-
-            
-            window.addEventListener("DOMContentLoaded", init);
+ 
 
