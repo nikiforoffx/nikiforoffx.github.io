577adab6769edb9f5be9c4147741061a68768f9f
nikiforoffx <63499133+nikiforoffx@users.noreply.github.com>
2020-04-11 22:03:35 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,15 +1,18 @@
-function init() {
+      <script type='text/javascript'>    
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
 
-Tabletop.init( 
-{ key: 'https://docs.google.com/spreadsheets/d/1itDmeN0nb5fdg4-k4HG5zE1hegS8mvwYcXnT0dQOfxM/pubhtml', 
-  
-  callback: function(data, tabletop) {
-  console.log(data) },
-    simpleSheet: true 
-    
-    } )
-}
+       var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1itDmeN0nb5fdg4-k4HG5zE1hegS8mvwYcXnT0dQOfxM/pubhtml';
 
-window.addEventListener(‘DOMContentLoaded’, init)
+            function init() {
+                Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+                            }
 
+            function showInfo(data, tabletop) {
+                alert('Successfully processed!')
+                      console.log(data);
+                                              }
+            window.addEventListener('DOMContentLoaded', init)
 
+      </script>
