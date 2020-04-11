0e59fd9777affd95d5a1bb1089b3939b54427233
nikiforoffx <63499133+nikiforoffx@users.noreply.github.com>
2020-04-11 21:37:39 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 function init() {
 
 Tabletop.init( 
-{ key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vQI1sEckR-HC4RtNneB85ejTala0Lbjys0vbiGAS8VS94ZD5ooJexM8KUIRi1Q90lSWNm0XufN44LBA/pubhtml',
+{ key: ‘https://docs.google.com/spreadsheets/d/1itDmeN0nb5fdg4-k4HG5zE1hegS8mvwYcXnT0dQOfxM/pubhtml',
   
   callback: function(data, tabletop) {
   console.log(data) },
@@ -10,4 +10,24 @@ Tabletop.init(
     } )
 }
 
+
+
 window.addEventListener(‘DOMContentLoaded’, init)
+
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
+<script type='text/javascript'>    
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1itDmeN0nb5fdg4-k4HG5zE1hegS8mvwYcXnT0dQOfxM/pubhtml';
+
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+  function showInfo(data, tabletop) {
+    alert('Successfully processed!')
+    console.log(data);
+  }
+
+  window.addEventListener('DOMContentLoaded', init)
+</script>
