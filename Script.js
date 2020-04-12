b09686cb735dc1efaa6f08b59c106d66f970ff04
nikiforoffx <63499133+nikiforoffx@users.noreply.github.com>
2020-04-12 10:57:56 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -8,7 +8,7 @@
                       console.log(data);
                 
                             for (var i = 0; i < data.length; i++) {
-                                    document.getElementById('googlelist').innerHTML = '<div ><h1>' + data[i].products + '</h1></div><br>';
+                                    $('googlelist').append( '<div ><h1>' + data[i].products + '</h1></div><br>');
                                                                   }
 
                                               }
